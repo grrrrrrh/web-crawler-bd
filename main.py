63efed9967b96730e0d0d08b317ec81d957c2b93
nd <ndl+github@mailbox.org>
2026-01-01 12:50:50 +0100

    Add CLI argument handling for base URL

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-    print("Hello from web-crawler-bd!")
+from __future__ import annotations
+
+import sys
+
+
+def main() -> None:
+    argv = sys.argv
+
+    if len(argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+
+    if len(argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    base_url = argv[1]
+    print(f"starting crawl of: {base_url}")
 
 
 if __name__ == "__main__":
